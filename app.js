704cac5e5a89059014bf480abb9a466d3b7eace9
Dheeraj <dheeraj8483@gmail.com>
2025-04-22 17:41:34 +0530

    added discount

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/",indexRouter);
 app.use("/owners",ownersRouter);
